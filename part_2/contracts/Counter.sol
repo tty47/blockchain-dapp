33d5258cd0524b3cd10d6351c2817f9c4c9b29da
JOSÉ RAMÓN MAÑES BLASCO <joseramon.manesblasco@viajeseci.es>
2021-01-22 20:05:17 +0100

    remove comments

--- part_2/contracts/Counter.sol
+++ part_2/contracts/Counter.sol
@@ -4,10 +4,8 @@ pragma solidity >=0.7.6 <0.8.0;
 contract Counter {
     // set as public to being able to know the amount
     uint public account;
-
     // create a new event in order to notify our methods
     event ChangedAccount(uint _account);
-
 
     constructor(uint _account){
         account = _account;
